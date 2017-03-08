109cfe2305555a10b44b44b10d6d6585b65dbc46
SWAGEAGLE <safwankath@gmail.com>
2017-03-07 22:53:18 -0500

    new

--- admin/schema.sql
+++ admin/schema.sql
@@ -1,6 +1,6 @@
+drop table appuser;
+drop table scores;
+drop table challenges;
 create table appuser(fname varchar(20), lname varchar(20), username varchar(20), passwd varchar(20), email varchar(20), numgamesplayed int, lastlogin date);
-
-insert into appuser(fname, lname, username, passwd, email, numgamesplayed, lastlogin) values ('Bob', 'Smith', 'bobby', 'bob', 'bob@example.com', 1, null);
 create table scores(username varchar(20), score int, scoretime date);
-insert into scores (username, score, scoretime) values ('bobby', 20, null);
 create table challenges(challenger varchar(20), opponent varchar(20), result varchar(20));
